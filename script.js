1651f2b488f0a3b9665107837e1fa781184990dd
Doonywoony <javismcarr@gmail.com>
2021-11-09 13:25:07 -0500

    variables defined and beginning functions declared

---
+++ script.js
@@ -0,0 +1,30 @@
+// Assignment Code
+//variable inputs for user
+var enter;
+var confirmNumber;
+var confirmUppercase;
+var confirmLowercase;
+var confirmCharacter;
+
+//numeric characters
+number = [ 1, 2, 3, 4, 5, 6, 7, 8, 9];
+//alphabetical characters
+alpha = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+//special characters
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
+
+
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
